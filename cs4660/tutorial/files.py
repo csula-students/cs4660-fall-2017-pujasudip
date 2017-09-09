9c26ca7bc7bb6b25db422ef34f5f2ead413a328c
Sudip Baral <pujasudip@gmail.com>
2017-09-09 15:59:14 -0700

    final correction exercise for file

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -11,10 +11,13 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        print("\npath: ", file_path)
         f = open(file_path, encoding='utf-8')
         text = f.read()
         lines = text.split('\n')
+        f.close()
 
+        print("array: \n", text)
         for line in lines:
             if len(line) > 0:
                 parts = list(map(int, line.split(' ')))
@@ -25,13 +28,16 @@ class SimpleFile(object):
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
+        print("mean: ", lists.get_avg(self.numbers[line_number]))
         return lists.get_avg(self.numbers[line_number])
+
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
+        print("max: ", max(self.numbers[line_number]))
         return max(self.numbers[line_number])
 
     def get_min(self, line_number):
@@ -39,6 +45,7 @@ class SimpleFile(object):
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
+        print("min: ", min(self.numbers[line_number]))
         return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
@@ -46,4 +53,5 @@ class SimpleFile(object):
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
+        print("sum: ", lists.get_sum(self.numbers[line_number]))
         return lists.get_sum(self.numbers[line_number])
